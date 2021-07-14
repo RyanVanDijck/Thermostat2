3420d91672bf569d76c7cab36f17e3dcd787d16e
RyanVanDijck <rvandijck02@outlook.com>
2021-07-14 11:40:44 +0100

    Cleaning up code

--- interface.js
+++ interface.js
@@ -1,44 +1,3 @@
-window.addEventListener('load', function() {
-  
-  // add a <button> element to your html with id of 'button'
-  const button = document.getElementById("button")
-
-  // add a <span> element to your html with id of 'message'
-  //const message = document.getElementById("message")
-
-  const power = document.getElementById("power")
-
-  const change = document.getElementById("change")
-
-  const reset = document.getElementById("reset")
-
-  const usage = document.getElementById("usage")
-  
-/*
-  button.addEventListener('click', function() {
-    message.innerHTML = thermostat.up();
-    usage.innerHTML = thermostat.getUsage();
-})done
-
-down.addEventListener('click', function() {
-  message.innerHTML = thermostat.down();
-  usage.innerHTML = thermostat.getUsage();
-})done
-
-change.addEventListener('click', function(){
-  thermostat.changePowerSaving();
-  power.innerHTML = thermostat.onPowerSaving();  
-})done
-
-reset.addEventListener('click', () => {
-  message.innerHTML = thermostat.reset(); 
-  usage.innerHTML = thermostat.getUsage();
-  
-})
-*/
-  
-})
-
 $( document ).ready(function() {
   const thermostat = new Thermostat();
   
@@ -62,7 +21,4 @@ $( document ).ready(function() {
     $("#message").text(thermostat.reset);
     $("#usage").text(thermostat.getUsage());
   })
-
-
-  
 });
